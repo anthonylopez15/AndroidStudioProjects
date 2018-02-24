9660ac6b46fba994bc43803497db0f440ab09838
Anthony Lopez <tony.lopez.15@gmail.com>
2018-02-24 19:09:08 -0400

    OnBoardingApp

---
+++ OnBoardingApp/app/src/main/java/br/com/exemple/samuchatappandroid/onboardingapp/MainActivity.java
@@ -0,0 +1,135 @@
+package br.com.exemple.samuchatappandroid.onboardingapp;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mSlideViewPger;
+    private LinearLayout mDotLayout;
+
+    private TextView[] mDots;
+
+    private SlideAdapter slideAdapter;
+
+    private Button mNextBtn;
+    private Button mBackBtn;
+
+    private int mCurrentPage;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSlideViewPger = findViewById(R.id.slideViewPager);
+        mDotLayout = findViewById(R.id.dotsLayout);
+
+        mNextBtn = findViewById(R.id.nextBtn);
+        mBackBtn = findViewById(R.id.prevBtn);
+
+
+
+        slideAdapter = new SlideAdapter(this);
+
+        mSlideViewPger.setAdapter(slideAdapter);
+
+        addDotsIdicator(0);
+
+        mSlideViewPger.addOnPageChangeListener(viewListener);
+
+        mNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mSlideViewPger.setCurrentItem(mCurrentPage + 1);
+
+            }
+        });
+
+        mBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mSlideViewPger.setCurrentItem(mCurrentPage - 1);
+
+            }
+        });
+
+
+    }
+
+    public void addDotsIdicator(int position){
+        mDots = new TextView[3];
+        mDotLayout.removeAllViews();
+
+        for (int i = 0; i < mDots.length; i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+
+            mDotLayout.addView(mDots[i]);
+
+        }
+
+        if( mDots.length > 0 ){
+            mDots[position].setTextColor(getResources().getColor(R.color.colorWhite));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+            addDotsIdicator(position);
+
+            mCurrentPage = position;
+
+            if (position == 0){
+
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(false);
+                mBackBtn.setVisibility(View.INVISIBLE);
+
+                mNextBtn.setText("Next");
+                mBackBtn.setText("");
+
+            }else if (position == mDots.length - 1){
+
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(true);
+                mBackBtn.setVisibility(View.VISIBLE);
+
+                mNextBtn.setText("Finish");
+                mBackBtn.setText("Back");
+
+            }else {
+
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(true);
+                mBackBtn.setVisibility(View.VISIBLE);
+
+                mNextBtn.setText("Next");
+                mBackBtn.setText("Back");
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+}
---
+++ OnBoardingApp/app/src/main/java/br/com/exemple/samuchatappandroid/onboardingapp/SlideAdapter.java
@@ -0,0 +1,73 @@
+package br.com.exemple.samuchatappandroid.onboardingapp;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Anthony on 24/02/2018.
+ */
+
+public class SlideAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SlideAdapter(Context context) {
+        this.context = context;
+    }
+
+    public int[] slide_images = {
+            R.drawable.eat_icon,
+            R.drawable.sleep_icon,
+            R.drawable.code_icon
+    };
+
+    public String[] slide_heading = { "EAT", "SLEEP", "CODE" };
+
+    public String[] slide_descriiption = {
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+    };
+
+    @Override
+    public int getCount() {
+        return slide_heading.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == (RelativeLayout) object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImageView = view.findViewById(R.id.slide_image);
+        TextView slideHeading = view.findViewById(R.id.slide_head);
+        TextView slideDescription = view.findViewById(R.id.slide_desc);
+
+        slideImageView.setImageResource(slide_images[position]);
+        slideHeading.setText(slide_heading[position]);
+        slideDescription.setText(slide_descriiption[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        container.removeView((RelativeLayout) object);
+
+    }
+}
